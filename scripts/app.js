056d37a416d035330d5cc0f0e518ba94cfc586b1
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-04-25 21:15:06 +0300

    hw30

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+// //1
+// const number = prompt('Number =')
+// function getFactorial (number) {
+//     if (number === 1 || number === 0){
+//         return 1
+//     } else {
+//         return number * getFactorial(number - 1)
+//     }
+// }
+// console.log(getFactorial(number))
+
+//2
+// const number2 = parseInt(prompt('Number'))
+// const degree = parseInt(prompt('Degree'))
+// function pow (number2, degree){
+//     if (isNaN(number2)|| isNaN(degree)){
+//         alert('some error')
+//     } else if(number2 === null || degree === null) {
+//         alert('enter the number')
+//     } else if (degree === 0) {
+//         return 1
+//     } else {
+//             return number2 * pow(number2, degree - 1)
+//         }
+// }
+// alert(pow(number2, degree))
+
+//3
+const a = prompt("Number A");
+const b = prompt("Number B");
